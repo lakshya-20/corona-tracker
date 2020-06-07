dca62f495a1d759d4b6e8bda3c366d15a247f6ba
Lakshya Bansal <bansallakshya94128@gmail.com>
2020-06-07 21:20:56 +0530

    added cards component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useMemo} from 'react';
 import './App.css';
 
 import {fetchData} from './api/index';
+import Cards from './components/cards/index'
 
 const App=()=> {
   const [data,setData]=useState({})
   const [country,setCountry]=useState("")
-  useEffect(()=>{
-    const data=fetchData();
-    setData(data)
+  useMemo(()=>{
+    const getData=async()=>{
+      const data=await fetchData();
+      setData(data)
+    }
+    getData()
   },[])
   
   const getCountryData=(country)=>{
-    const data=fetchData(country);
+    const data=fetchData(country)
     setData(data);
     setCountry(country);
   }
   
-  console.log(data)
+  
   return (
     <div className="App">
-      
+      <div className="top-wrapper">
+
+      </div>
+      <div className="content-wrapper">
+        <div className="left-wrapper">
+          <Cards data={data}/>
+        </div>
+        <div className="right-wrapper">
+          Right Wrapper
+        </div>      
+      </div>
     </div>
   );
 }
